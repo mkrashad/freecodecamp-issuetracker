52346c0c1c715b39e2c1195cbdc718759e30e626
Rashad Mahmudov <mkrashad25@gmail.com>
2024-04-10 11:39:12 +0200

    Establish connection to mongodb

---
+++ database/db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+class DB {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect(process.env['MONGO_URI'], {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log('Database connection successfully');
+      })
+      .catch((err) => {
+        console.error('Database connection error');
+      });
+  }
+}
+
+module.exports = new DB();
